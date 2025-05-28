6558212edeaff01cef4e6685899d6f914d364cf5
ShuShuicu <Tomori@miomoe.cn>
2025-05-29 04:54:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * 一款不错的Typecho主题
  * @package Uika
  * @author 鼠子(Tomoriゞ)
- * @version 1.0.0_Dev4
+ * @version 1.0.0_Beta1
  * @link https://github.com/ShuShuicu/Typecho-Uika-Theme
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
